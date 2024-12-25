82a18265aacaf5b2d0dec3f76a750748c64c53dd
Rithika P <166213945+Rithika2406@users.noreply.github.com>
2024-12-25 19:12:06 +0530

    Add files via upload

---
+++ earnamountandtax.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner S=new Scanner(System.in);
+		int t=S.nextInt();
+		while(t>0)
+		{
+		    int x=S.nextInt();
+		int y=S.nextInt();
+		    int z=x-y;
+		    System.out.println(z);
+		    t--;
+		}
+
+	}
+}
